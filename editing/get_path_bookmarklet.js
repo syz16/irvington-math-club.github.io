5c5fc797589276f4685d5a37b43d9b3676b32346
Jared Hughes <jahughes241@gmail.com>
2019-08-28 19:11:04 -0700

    Fix copy post path bookmarklet

--- editing/get_path_bookmarklet.js
+++ editing/get_path_bookmarklet.js
@@ -21,5 +21,5 @@ function copyToClipboard(text) {
 }
 
 let url = window.location.pathname;
-let path = url.match(/\/(blob|edit)\/master\/(.*)/)[1];
+let path = url.match(/\/(blob|edit)\/master\/(.*)/)[2];
 copyToClipboard(path)
